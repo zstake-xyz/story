92092133c01217fa691b2282cafa9f8b91ce2967
Zstake <hyunjubd@gmail.com>
2024-10-02 15:52:27 +0900

    Update story_installer.sh

--- story_installer.sh
+++ story_installer.sh
@@ -32,7 +32,7 @@ commands = [
     "sudo systemctl daemon-reload && sudo systemctl start story-geth && sudo systemctl enable story-geth",
     "sudo systemctl daemon-reload && sudo systemctl start story && sudo systemctl enable story",
     # Check block
-    "while true; do local_height=$(curl -s localhost:26657/status | jq -r '.result.sync_info.latest_block_height'); network_height=$(curl -s https://rpc-story.josephtran.xyz/status | jq -r '.result.sync_info.latest_block_height'); blocks_left=$((network_height - local_height)); echo -e \"\\033[1;38mYour node height:\\033[0m \\033[1;34m$local_height\\033[0m | \\033[1;35mNetwork height:\\033[0m \\033[1;36m$network_height\\033[0m | \\033[1;29mBlocks left:\\033[0m \\033[1;31m$blocks_left\\033[0m\"; sleep 5; done"
+    "while true; do local_height=$(curl -s localhost:26657/status | jq -r '.result.sync_info.latest_block_height'); network_height=$(curl -s https://story-testnet-rpc.zstake.xyz/status | jq -r '.result.sync_info.latest_block_height'); blocks_left=$((network_height - local_height)); echo -e \"\\033[1;38mYour node height:\\033[0m \\033[1;34m$local_height\\033[0m | \\033[1;35mNetwork height:\\033[0m \\033[1;36m$network_height\\033[0m | \\033[1;29mBlocks left:\\033[0m \\033[1;31m$blocks_left\\033[0m\"; sleep 5; done"
 ]
 
 for command in commands:
